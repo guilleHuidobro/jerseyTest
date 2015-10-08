cac5a2430daec12cdb105cdf80902ce1d21a078f
guille huidobro <ghuidobro@belatrixsf.com>
2015-10-08 17:09:27 -0300

    first commit examples

---
+++ src/test/jersey/resource/HelloWorldResource.java
@@ -0,0 +1,34 @@
+package test.jersey.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/helloworld")
+public class HelloWorldResource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayPlainTextHello() {
+		return "Hello World RESTful Jersey!";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String sayXMLHello() {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello World RESTful Jersey"
+				+ "</hello>";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String sayHtmlHello() {
+		return "<html> " + "<title>" + "Hello World RESTful Jersey"
+				+ "</title>" + "<body><h1>" + "Hello World RESTful Jersey"
+				+ "</body></h1>" + "</html> ";
+	}
+
+}
